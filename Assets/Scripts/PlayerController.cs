7de1638531a0c629ad68fe474165ac213031ad9b
Kito <140571205+0Kid@users.noreply.github.com>
2024-07-18 10:35:34 +0700

    Update Spawn Ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     public float jumpForce = 700f;
     private Rigidbody2D rb;
+    private bool firstCollision = true;    
 
     void Start()
     {
@@ -20,7 +21,21 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        rb.velocity = Vector2.zero;
-        rb.AddForce(new Vector2(0, jumpForce));
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (firstCollision)
+            {
+                // Hindari spawn platform pada collision pertama
+                firstCollision = false;
+                return;
+            }            
+            // Panggil metode untuk meng-spawn ground baru
+            FindObjectOfType<GroundSpawner>().SpawnPlatform();
+        }
     }    
 }
